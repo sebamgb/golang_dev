6613bcdd1f530440b1cafa3a3fa7bb07734e3e76
seba <sebamgb@gmail.com>
2022-04-30 13:55:24 -0400

    utizando arrays y slices en golang

--- src/main.go
+++ src/main.go
@@ -5,21 +5,29 @@ import (
 )
 
 func main() {
-	// Defer
-	defer fmt.Println("hola")
-	fmt.Println("mundo")
+	// Arrays:
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// Continue y break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		if i == 2 {
-			fmt.Println("es dos")
-			continue
-		} else {
-			if i == 8 {
-				fmt.Println("break")
-				break
-			}
-		}
-	}
+	// Slices:
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])  //último elemento exclusivo
+	fmt.Println(slice[2:4]) //primer elemento inclusivo y último exclusivo
+	fmt.Println(slice[4:])  //primer elemento inclusivo
+
+	// array inmutables, sin embargo silces no
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append nueva lista
+	new_slice := []int{8, 9, 10}
+	slice = append(slice, new_slice...)
+	fmt.Println(slice)
 }
