8d99838ff936df15c10bb66e3bf5a4dbda5938e1
seba <sebamgb@gmail.com>
2022-04-29 20:48:33 -0400

    condicionales anidadas con switch

--- src/main.go
+++ src/main.go
@@ -4,59 +4,22 @@ import (
 	"fmt"
 )
 
-// Reto condicionales:
-//authentication
-func auth(u, p string) {
-	user := "seba"
-	password := "1234"
-	if u == user && p == password {
-		fmt.Println("access")
-	} else {
-		fmt.Println("access denied")
-	}
-}
-
-func par(a uint) {
-	fmt.Println(a)
-	if a%2 == 0 {
-		fmt.Println("es par")
-	} else {
-		fmt.Print("no es par")
-	}
-}
 func main() {
-	// Condicional if
-	v := 2
-	if v == 1 {
-		fmt.Println("es:", v)
-	} else {
-		fmt.Println("distinto a:", v)
+	// condiciones anidadas con switch
+	switch mod := 2 % 2; mod {
+	case 0:
+		fmt.Println("es par")
+	default:
+		fmt.Println("no es par")
 	}
-	// with and
-	v2 := 3
-	if v == 2 && v2 != 3 {
-		fmt.Println("Los dos se cumplen")
-	} else {
-		fmt.Println("uno o los dos no se cumplen")
+	//sin condicion
+	value := 100
+	switch {
+	case value < 100:
+		fmt.Println("es menor a 100")
+	case value > 100:
+		fmt.Println("es mayor a 100")
+	default:
+		fmt.Println("es 100")
 	}
-	// with or
-	if v != 2 || v2 == 2 {
-		fmt.Println("Uno o los dos se cumplen")
-	} else {
-		fmt.Println("No se cumple ninguno")
-	}
-	// ->Manejo de errores con if
-	// Convertir texto a numero
-	//value, err := strconv.Atoi("ahcagxh")
-	// nil: no existe error
-	//if err != nil {
-	//	// log fatal luego de mostrar error termina el programa
-	//	log.Fatal(err)
-	//} else {
-	//	fmt.Println("value:", value)
-	//}
-	fmt.Println("par?:")
-	par(1)
-	fmt.Println()
-	auth("seba", "1324")
 }
