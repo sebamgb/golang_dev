33b3d6009a24eb3b14dfe127ad458218909b720b
seba <sebamgb@gmail.com>
2022-05-09 20:18:53 -0400

    modificadores de acceso en funcines y structs

--- src/main.go
+++ src/main.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
-
-type car struct {
-	brand string
-	year  int
-}
+import (
+	"fmt"
+	pk "golang/src/mypackage"
+)
 
 func main() {
-	// Instanciado
-	my_car := car{brand: "ford", year: 2022}
+	// instanciando struct publico
+	var my_car pk.CarPublic
+	// struct inaccesible
+	//var my_car pk.carPrivate
+
 	fmt.Println(my_car)
 
-	// Otra manera
-	var another_car car
-	another_car.brand = "ferrari"
-	fmt.Println(another_car) //al no especificar todo el contenido del struct imrime los zero values del tipo de dato
+	pk.PrintMessge("Hola Sebastián")
+
+	// función inaccesible
+	//pk.printMessage("hola")
 }
---
+++ src/mypackage/mypackage.go
@@ -0,0 +1,24 @@
+package mypackage
+
+import "fmt"
+
+// CarPublic struct de de acceso publico por la mayúscula
+type CarPublic struct {
+	Brand string
+	Year  int
+}
+
+// structs y funciones de acceso priado por comenzar con minúscula
+type carPrivate struct {
+	brand string
+	year  int
+}
+
+// PrintMessage imprime un mensaje con acceso público
+func PrintMessge(text string) {
+	fmt.Println(text)
+}
+
+func printMessge(text string) {
+	fmt.Println(text)
+}
