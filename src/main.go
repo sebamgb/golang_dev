1808b88ef2f9e1f1e60301513f8b78f087c7a7ba
seba <sebamgb@gmail.com>
2022-05-14 22:24:13 -0400

    keywords close, range, select con channels en golang

--- src/main.go
+++ src/main.go
@@ -4,28 +4,41 @@ import (
 	"fmt"
 )
 
-// ejemplificando channels otra forma más usada de organizar las goroutines
-
-//-creamos una función
-//-con un parametro para un channel que recibe como entrada strings
-//-con afán de hacer más eficiente el código le indicamos si el channel va a ser de entrada o salida
-//entrada: chan<- typedata
-//salida: <-chan typedata
-func say(t string, c chan<- string) {
-	//ingresando string t en canal
-	//salida: t = <- chan
+func m(t string, c chan string) {
 	c <- t
 }
-
 func main() {
-	//--declaramos un channel--
-	//-channel que maneja tipos de datos string y recibe una goroutine a la vez, si no se le indica el número de goroutines que maneje va a elegir uno dinamicamente
-	c := make(chan string, 1)
-	fmt.Println("hello")
-	go say("bye", c)
-	//-imprimimos la salida del dato en el canal
-	fmt.Println(<-c)
-}
+	c := make(chan string, 2)
+	c <- "mensaje1"
+	c <- "mensaje2"
+	//==range y close==
+	//cerramos el channel con close cuando esta lleno siendo una buena practica que optimiza más
+	close(c)
+	fmt.Println(len(c), cap(c))
+	//-una vez cerrado no se pueden agregar más datos aunque le sobre espacio
+	//c<- "mensaje3"
 
-// primitivos o wait group para una optimizacion en el codigo
-// chanels más livianos de codigo para tareas que no necesiten tanta optimización
+	//recorremos el channel en busca de los datos
+	//-al no cerrar el channel antes de recorrer se siguen esperando datos
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//==select==
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go m("mensaje1", email1)
+	go m("mensaje2", email2)
+
+	//es bueno saber cuantos canales estas usando para la condicional del for
+	for i := 0; i < 2; i++ {
+		//select muestra dos channels completamente independientes
+		//no hay certesa de cual va a mostrar, pero si enumeramos los nombres de los channels va a tomar los channels de forma descendente, es decir los numeros mas altos primero
+		select {
+		case m1 := <-email1:
+			fmt.Println("email recibido de:", m1)
+		case m2 := <-email2:
+			fmt.Println("email recibido de:", m2)
+		}
+	}
+}
